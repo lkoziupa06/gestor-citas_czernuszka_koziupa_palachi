c0893d81e05348dd49c3bf43151c62afb329fc2e
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-06-05 14:04:12 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {useState} from 'React'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [citas, setCitas] = useState([]);
   return (
     <>
     
